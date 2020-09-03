9bbeeca79a106d621f4ae84f82b5f2b065b17461
Dane Taylor <danet@buffalo.edu>
2020-09-03 16:27:59 -0600

    Add files via upload

--- HW0/docs/source/conf.py
+++ HW0/docs/source/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../code'))
+sys.path.insert(0, os.path.abspath('../../notebooks'))
 sys.setrecursionlimit(1000)
 
 # -- Project information -----------------------------------------------------
