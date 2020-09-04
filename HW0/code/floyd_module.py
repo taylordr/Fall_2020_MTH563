946260aa164c3be60febdb0c51af23290b94cdc5
Floyd <36140953+ziangzha@users.noreply.github.com>
2020-09-04 14:17:45 -0500

    Create floyd_module.py

---
+++ HW0/code/floyd_module.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def square_a_number(x):
+	"""
+    Dane's first function.
+    Args:
+        x (float): Some number.
+    Returns:
+        float: Returns :math:`y=x^2`
+    """
+
+	return x**2
+
+def times_by_3(x):
+	"""
+    Dane's second function.
+    Args:
+        x (float): Some number.
+    Returns:
+        float: Returns :math:`y=3x`
+    """
+
+	return 3*x
