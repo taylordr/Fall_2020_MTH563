50598a5cd77035a12b041ef69a4f998d41c07c50
Tong Wu <50079185+ameliawu17@users.noreply.github.com>
2020-09-04 14:55:05 -0400

    Add files via upload

---
+++ HW0/code/tong_module.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+def double_a_number(x):
+    """
+    The first function.
+    
+    Args:
+        x (float): Some number.
+    Returns:
+        float: Returns :math:`y=x*2`
+    
+    """
+    return x*2
+
+def ten_times_greater(x):
+    """
+    The second function.
+    
+    Args:
+        x (float): Some number.
+    Returns:
+        float: Returns :math:`y=x*10`
+        
+    """
+    return x*10
+
+
+# In[ ]:
+
+
+
+
